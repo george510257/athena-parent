bc0398691f4721f5ec18e56e1d6a2253c66227f7
李智勇 <lizy19@xiaopeng.com>
2024-07-12 09:29:59 +0800

    update starter data jpa

--- athena-project/athena-starter/athena-starter-data-jpa/src/main/java/com/athena/starter/data/jpa/DataJpaAutoConfig.java
+++ athena-project/athena-starter/athena-starter-data-jpa/src/main/java/com/athena/starter/data/jpa/DataJpaAutoConfig.java
@@ -1,9 +1,14 @@
 package com.athena.starter.data.jpa;
 
+import com.athena.common.core.constant.BaseConstants;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @ComponentScan
+@EntityScan(basePackages = BaseConstants.BASE_PACKAGE_PREFIX + ".**.entity")
+@EnableJpaRepositories(basePackages = BaseConstants.BASE_PACKAGE_PREFIX + ".**.repository")
 public class DataJpaAutoConfig {
 }
