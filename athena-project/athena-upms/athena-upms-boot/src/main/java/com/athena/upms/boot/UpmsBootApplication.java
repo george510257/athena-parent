f6ef1a76fdccd0c14ddd90a9fbd41cc7d5b58e36
李智勇 <lizy19@xiaopeng.com>
2024-07-18 14:02:36 +0800

    add upms

---
+++ athena-project/athena-upms/athena-upms-boot/src/main/java/com/athena/upms/boot/UpmsBootApplication.java
@@ -0,0 +1,12 @@
+package com.athena.upms.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpmsBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpmsBootApplication.class, args);
+    }
+}
