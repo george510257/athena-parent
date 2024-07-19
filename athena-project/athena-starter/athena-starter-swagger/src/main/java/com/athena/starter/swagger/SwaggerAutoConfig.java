34ab77efe78af1d499744b4be2b44c1e419269f6
李智勇 <lizy19@xiaopeng.com>
2024-07-19 13:41:34 +0800

    update swagger

--- athena-project/athena-starter/athena-starter-swagger/src/main/java/com/athena/starter/swagger/SwaggerAutoConfig.java
+++ athena-project/athena-starter/athena-starter-swagger/src/main/java/com/athena/starter/swagger/SwaggerAutoConfig.java
@@ -1,12 +1,9 @@
 package com.athena.starter.swagger;
 
-import com.athena.starter.swagger.config.SwaggerProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
-@EnableConfigurationProperties({SwaggerProperties.class})
 public class SwaggerAutoConfig {
 }
