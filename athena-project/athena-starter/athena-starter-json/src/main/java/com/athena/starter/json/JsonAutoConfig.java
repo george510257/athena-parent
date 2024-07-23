2a58bb9b70e1c8cbe4e023017fa1fd98bfb6eeb0
李智勇 <lizy19@xiaopeng.com>
2024-07-23 15:41:28 +0800

    update athena starter json

--- athena-project/athena-starter/athena-starter-json/src/main/java/com/athena/starter/json/JsonAutoConfig.java
+++ athena-project/athena-starter/athena-starter-json/src/main/java/com/athena/starter/json/JsonAutoConfig.java
@@ -1,19 +1,9 @@
 package com.athena.starter.json;
 
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 
 @Configuration
 @ComponentScan
 public class JsonAutoConfig {
-
-    @Bean
-    @Order(Ordered.HIGHEST_PRECEDENCE)
-    public JavaTimeModule javaTimeModule() {
-        return new JavaTimeModule();
-    }
 }
---
+++ athena-project/athena-starter/athena-starter-json/src/main/java/com/athena/starter/json/config/JacksonConfig.java
@@ -0,0 +1,80 @@
+package com.athena.starter.json.config;
+
+import cn.hutool.core.date.DatePattern;
+import com.athena.starter.json.support.DefaultDateFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
+
+/**
+ * Jackson 配置
+ */
+@Configuration
+public class JacksonConfig {
+
+    /**
+     * Jackson 全局配置
+     *
+     * @return 自定义配置类
+     */
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer(List<Module> modules) {
+        return builder -> {
+            // 设置全局地区
+            builder.locale(Locale.CHINA);
+            // 设置全局时区
+            builder.timeZone(TimeZone.getTimeZone(ZoneId.systemDefault()));
+            // 设置全局日期格式
+            builder.dateFormat(new DefaultDateFormat());
+            // 设置全局序列化模块 JavaTimeModule 用于序列化和反序列化 java8 时间类型
+            builder.modules(modules);
+            // 反序列化时忽略未知属性
+            builder.failOnUnknownProperties(false);
+            // 序列化时忽略值为 null 的属性
+            builder.serializationInclusion(JsonInclude.Include.ALWAYS);
+        };
+    }
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public JavaTimeModule javaTimeModule() {
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        // ======================= 时间序列化规则 ===============================
+        // yyyy-MM-dd HH:mm:ss
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DatePattern.NORM_DATETIME_FORMATTER));
+        // yyyy-MM-dd
+        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DatePattern.NORM_DATE_FORMATTER));
+        // HH:mm:ss
+        javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DatePattern.NORM_TIME_FORMATTER));
+        // Long 类型转换为 String 类型
+        javaTimeModule.addSerializer(Long.class, ToStringSerializer.instance);
+        // ======================= 时间反序列化规则 ==============================
+        // yyyy-MM-dd HH:mm:ss
+        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DatePattern.NORM_DATETIME_FORMATTER));
+        // yyyy-MM-dd
+        javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DatePattern.NORM_DATE_FORMATTER));
+        // HH:mm:ss
+        javaTimeModule.addDeserializer(LocalTime.class, new LocalTimeDeserializer(DatePattern.NORM_TIME_FORMATTER));
+        return javaTimeModule;
+    }
+}
