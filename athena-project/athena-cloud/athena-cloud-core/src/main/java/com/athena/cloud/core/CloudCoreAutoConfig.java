dc47098eee77b45992065942230c9e5ebe24ff6b
李智勇 <lizy19@xiaopeng.com>
2024-07-24 15:08:47 +0800

    add athena security

---
+++ athena-project/athena-cloud/athena-cloud-core/src/main/java/com/athena/cloud/core/CloudCoreAutoConfig.java
@@ -0,0 +1,12 @@
+package com.athena.cloud.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * cloud core auto config
+ */
+@Configuration
+@ComponentScan
+public class CloudCoreAutoConfig {
+}
--- athena-project/athena-starter/athena-starter-json/src/main/java/com/athena/starter/json/config/JacksonConfig.java
+++ athena-project/athena-starter/athena-starter-json/src/main/java/com/athena/starter/json/config/JacksonConfig.java
@@ -3,7 +3,6 @@ package com.athena.starter.json.config;
 import cn.hutool.core.date.DatePattern;
 import com.athena.starter.json.support.DefaultDateFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
@@ -22,7 +21,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneId;
-import java.util.List;
 import java.util.Locale;
 import java.util.TimeZone;
 
@@ -38,7 +36,7 @@ public class JacksonConfig {
      * @return 自定义配置类
      */
     @Bean
-    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer(List<Module> modules) {
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
         return builder -> {
             // 设置全局地区
             builder.locale(Locale.CHINA);
@@ -46,8 +44,6 @@ public class JacksonConfig {
             builder.timeZone(TimeZone.getTimeZone(ZoneId.systemDefault()));
             // 设置全局日期格式
             builder.dateFormat(new DefaultDateFormat());
-            // 设置全局序列化模块 JavaTimeModule 用于序列化和反序列化 java8 时间类型
-            builder.modules(modules);
             // 反序列化时忽略未知属性
             builder.failOnUnknownProperties(false);
             // 序列化时忽略值为 null 的属性
