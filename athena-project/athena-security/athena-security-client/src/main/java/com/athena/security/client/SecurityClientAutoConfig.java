78f80e69451d595f38a859344fe9827ad684de01
李智勇 <lizy19@xiaopeng.com>
2024-07-24 15:42:26 +0800

    add athena security

---
+++ athena-project/athena-security/athena-security-client/src/main/java/com/athena/security/client/SecurityClientAutoConfig.java
@@ -0,0 +1,9 @@
+package com.athena.security.client;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class SecurityClientAutoConfig {
+}
