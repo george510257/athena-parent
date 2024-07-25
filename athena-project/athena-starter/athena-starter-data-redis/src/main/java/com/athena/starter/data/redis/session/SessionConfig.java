5894206eeed1037266c500897cac47e358256e46
李智勇 <lizy19@xiaopeng.com>
2024-07-25 15:53:01 +0800

    add athena security

--- athena-project/athena-starter/athena-starter-data-redis/src/main/java/com/athena/starter/data/redis/session/SessionConfig.java
+++ athena-project/athena-starter/athena-starter-data-redis/src/main/java/com/athena/starter/data/redis/session/SessionConfig.java
@@ -1,10 +1,14 @@
 package com.athena.starter.data.redis.session;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
@@ -28,7 +32,11 @@ public class SessionConfig {
     @ConditionalOnMissingBean
     public RedisSerializer<Object> jsonRedisSerializer(Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder) {
         ObjectMapper objectMapper = jackson2ObjectMapperBuilder.build();
-        return new GenericJackson2JsonRedisSerializer(objectMapper);
+        // 设置可见度
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        // 启用默认类型
+        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        return new Jackson2JsonRedisSerializer<>(objectMapper, Object.class);
     }
 
 }
