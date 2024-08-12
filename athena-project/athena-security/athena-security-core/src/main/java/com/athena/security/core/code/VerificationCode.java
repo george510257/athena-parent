c4c33f3f7d4ecb6fc16ae0f4c6d3b47497ed3883
李智勇 <george510257@gmail.com>
2024-08-12 22:32:59 +0800

    添加验证码校验

---
+++ athena-project/athena-security/athena-security-core/src/main/java/com/athena/security/core/code/VerificationCode.java
@@ -0,0 +1,21 @@
+package com.athena.security.core.code;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 验证码
+ */
+@Data
+public abstract class VerificationCode implements Serializable {
+    /**
+     * 验证码
+     */
+    private String code;
+    /**
+     * 过期时间
+     */
+    private Date expireTime;
+}
---
+++ athena-project/athena-security/athena-security-core/src/main/java/com/athena/security/core/code/VerificationCodeType.java
@@ -0,0 +1,17 @@
+package com.athena.security.core.code;
+
+/**
+ * 验证码类型
+ */
+public enum VerificationCodeType {
+
+    /**
+     * 短信验证码
+     */
+    SMS,
+    /**
+     * 图形验证码
+     */
+    IMAGE
+
+}
