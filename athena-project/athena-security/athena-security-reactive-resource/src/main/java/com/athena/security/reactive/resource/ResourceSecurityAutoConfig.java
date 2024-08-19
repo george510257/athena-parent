aa61a6460261535c68c0429b313ecfc799c19122
george1215 <george510257@gmail.com>
2024-08-19 09:10:47 +0800

    添加resource server

---
+++ athena-project/athena-security/athena-security-reactive-resource/src/main/java/com/athena/security/reactive/resource/ResourceSecurityAutoConfig.java
@@ -0,0 +1,9 @@
+package com.athena.security.reactive.resource;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class ResourceSecurityAutoConfig {
+}
