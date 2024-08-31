252d72c31ab99ad0bc47de987bd76eec5c5954bb
李智勇 <george510257@gmail.com>
2024-08-31 17:41:33 +0800

    提取配置项目

--- athena-project/athena-security/athena-security-servlet-client/src/main/java/com/athena/security/servlet/client/delegate/IUserResponseConverter.java
+++ athena-project/athena-security/athena-security-servlet-client/src/main/java/com/athena/security/servlet/client/delegate/IUserResponseConverter.java
@@ -10,4 +10,24 @@ import java.util.function.Predicate;
  * 用户响应转换器
  */
 public interface IUserResponseConverter extends Converter<OAuth2UserRequest, Converter<Map<String, Object>, Map<String, Object>>>, Predicate<String> {
+
+    /**
+     * 测试是否支持指定的注册标识
+     *
+     * @param oauth2UserRequest 用户请求
+     * @return 是否支持
+     */
+    @Override
+    default Converter<Map<String, Object>, Map<String, Object>> convert(OAuth2UserRequest oauth2UserRequest) {
+        return params -> convert(oauth2UserRequest, params);
+    }
+
+    /**
+     * 转换
+     *
+     * @param oauth2UserRequest 用户请求
+     * @param params            参数
+     * @return 转换结果
+     */
+    Map<String, Object> convert(OAuth2UserRequest oauth2UserRequest, Map<String, Object> params);
 }
--- athena-project/athena-security/athena-security-servlet-client/src/main/java/com/athena/security/servlet/client/feishu/FeishuUserResponseConverter.java
+++ athena-project/athena-security/athena-security-servlet-client/src/main/java/com/athena/security/servlet/client/feishu/FeishuUserResponseConverter.java
@@ -3,7 +3,6 @@ package com.athena.security.servlet.client.feishu;
 import com.athena.security.servlet.client.delegate.IUserResponseConverter;
 import com.athena.security.servlet.client.feishu.domian.FeishuProperties;
 import jakarta.annotation.Resource;
-import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.stereotype.Component;
 
@@ -33,25 +32,15 @@ public class FeishuUserResponseConverter implements IUserResponseConverter {
     }
 
     /**
-     * 转换为响应实体
-     *
-     * @param source 用户请求
-     * @return 响应实体
-     */
-    @Override
-    public Converter<Map<String, Object>, Map<String, Object>> convert(OAuth2UserRequest source) {
-        // 转换为响应实体
-        return this::convert;
-    }
-
-    /**
      * 转换
      *
-     * @param params 参数
+     * @param oauth2UserRequest 用户请求
+     * @param params            参数
      * @return 转换结果
      */
-    private Map<String, Object> convert(Map<String, Object> params) {
-        // 返回 data 节点数据
+    @Override
+    public Map<String, Object> convert(OAuth2UserRequest oauth2UserRequest, Map<String, Object> params) {
+        // 获取数据
         return (Map<String, Object>) params.get("data");
     }
 }
