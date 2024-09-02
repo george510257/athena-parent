e8e4b1b7e07a79742aeb9233e159fd7d9f6fcbbf
george1215 <george510257@gmail.com>
2024-09-02 13:45:01 +0800

    优化 oauth2 client

--- athena-project/athena-security/athena-security-servlet-client/src/main/java/com/athena/security/servlet/client/customizer/AuthorizationCodeGrantCustomizer.java
+++ athena-project/athena-security/athena-security-servlet-client/src/main/java/com/athena/security/servlet/client/customizer/AuthorizationCodeGrantCustomizer.java
@@ -1,5 +1,8 @@
 package com.athena.security.servlet.client.customizer;
 
+import com.athena.security.servlet.client.delegate.DelegateAuthorizationCodeTokenResponseClient;
+import com.athena.security.servlet.client.delegate.DelegateAuthorizationRequestResolver;
+import jakarta.annotation.Resource;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2ClientConfigurer;
@@ -13,11 +16,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class AuthorizationCodeGrantCustomizer implements Customizer<OAuth2ClientConfigurer<HttpSecurity>.AuthorizationCodeGrantConfigurer> {
     /**
+     * 委托授权请求解析器
+     */
+    @Resource
+    private DelegateAuthorizationRequestResolver delegateAuthorizationRequestResolver;
+    /**
+     * 委托授权码令牌响应客户端
+     */
+    @Resource
+    private DelegateAuthorizationCodeTokenResponseClient delegateAuthorizationCodeTokenResponseClient;
+
+    /**
      * 自定义 OAuth2 授权码授权配置
      *
      * @param configurer 配置器
      */
     @Override
     public void customize(OAuth2ClientConfigurer<HttpSecurity>.AuthorizationCodeGrantConfigurer configurer) {
+        // 设置委托授权请求解析器
+        configurer.authorizationRequestResolver(delegateAuthorizationRequestResolver);
+        // 设置委托授权码令牌响应客户端
+        configurer.accessTokenResponseClient(delegateAuthorizationCodeTokenResponseClient);
     }
 }
