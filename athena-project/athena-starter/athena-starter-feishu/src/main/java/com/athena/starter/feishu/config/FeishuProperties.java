5d1c7e3159175d6ddf38b16e8f424a04d1157989
george1215 <george510257@gmail.com>
2024-10-24 15:24:01 +0800

    添加 feishu sdk starter

---
+++ athena-project/athena-starter/athena-starter-feishu/src/main/java/com/athena/starter/feishu/config/FeishuProperties.java
@@ -0,0 +1,58 @@
+package com.athena.starter.feishu.config;
+
+import com.athena.common.core.constant.BaseConstants;
+import com.athena.common.core.constant.BaseProperties;
+import com.lark.oapi.core.enums.AppType;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 飞书配置
+ *
+ * @author george
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ConfigurationProperties(prefix = BaseConstants.BASE_PROPERTIES_PREFIX + ".feishu")
+public class FeishuProperties extends BaseProperties {
+    /**
+     * 应用appid
+     */
+    private String appId;
+    /**
+     * 应用密钥
+     */
+    private String appSecret;
+    /**
+     * 应用类型: 企业应用、应用商店应用
+     */
+    private AppType appType = AppType.SELF_BUILT;
+    /**
+     * 调试标识
+     */
+    private boolean debugFlag = false;
+    /**
+     * token缓存标识
+     */
+    private boolean tokenCacheFlag = true;
+    /**
+     * 服务台id
+     */
+    private String helpDeskId;
+    /**
+     * 服务台密钥
+     */
+    private String helpDeskSecret;
+    /**
+     * 请求超时时间
+     */
+    private long requestTimeout = 10;
+    /**
+     * 超时时间单位
+     */
+    private TimeUnit requestTimeoutUnit = TimeUnit.SECONDS;
+
+}
