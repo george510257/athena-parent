8da5c378c66a0fa0382bb45862b8812c8588f2bd
george1215 <george510257@gmail.com>
2024-11-29 15:09:33 +0800

    获取登录用户信息

--- athena-project/athena-security/athena-security-servlet-client/src/main/java/com/gls/athena/security/servlet/client/social/SocialUser.java
+++ athena-project/athena-security/athena-security-servlet-client/src/main/java/com/gls/athena/security/servlet/client/social/SocialUser.java
@@ -1,6 +1,8 @@
 package com.gls.athena.security.servlet.client.social;
 
+import com.gls.athena.common.bean.base.BaseVo;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
@@ -13,7 +15,8 @@ import java.util.Set;
  * @author george
  */
 @Data
-public class SocialUser implements OAuth2User {
+@EqualsAndHashCode(callSuper = true)
+public class SocialUser extends BaseVo implements OAuth2User {
     /**
      * 权限
      */
