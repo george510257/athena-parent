1f9d26b81694eabe2a1c864ebc589e2efcdf0ea9
george <george510257@gmail.com>
2024-12-05 23:11:55 +0800

    添加短信处理

---
+++ athena-project/athena-starter/athena-starter-sms/src/main/java/com/gls/athena/starter/sms/SmsAutoConfig.java
@@ -0,0 +1,14 @@
+package com.gls.athena.starter.sms;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 短信自动配置
+ *
+ * @author george
+ */
+@Configuration
+@ComponentScan
+public class SmsAutoConfig {
+}
